0221ebe16dae6ec0ecb7ddc94a1db71e85c9dee5
Sawindu Samuditha <sawindusamuditha@gmail.com>
2024-11-28 07:44:55 +0530

    Create Apply Advance Loading page
    Create Apply Advance Loading page all created click Listener available

--- app/src/main/java/com/evergreen/eve/Apply_AdvanceLoadPage.java
+++ app/src/main/java/com/evergreen/eve/Apply_AdvanceLoadPage.java
@@ -2,7 +2,7 @@ package com.evergreen.eve;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Apply_AdvanceMain extends AppCompatActivity {
+public class Apply_AdvanceLoadPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_apply_advance_main);
+        setContentView(R.layout.activity_apply_advance_load_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Apply_AdvanceLoadPage apply_advanceLoadPage = new Apply_AdvanceLoadPage();
-        Button button = findViewById(R.id.btnApplyAdvance);
+        Apply_AdvanceMain apply_advanceMain = new Apply_AdvanceMain();
+        ImageView imageView = findViewById(R.id.loadApplyAdvanceBack);
 
-        button.setOnClickListener(v -> {
-            startActivity(new Intent(Apply_AdvanceMain.this, Apply_AdvanceLoadPage.class));
+        imageView.setOnClickListener(v -> {
+            startActivity(new Intent(Apply_AdvanceLoadPage.this, Apply_AdvanceMain.class));
         });
     }
 }
