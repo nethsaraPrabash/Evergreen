d4c3d1a0cea6e86d84c0a398dc4a2aca5da71260
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2025-01-04 13:27:47 +0530

    Camera added

--- app/src/main/java/com/evergreen/eve/ProfileActivity.kt
+++ app/src/main/java/com/evergreen/eve/ProfileActivity.kt
@@ -12,12 +12,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
+import java.net.URL
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var profilePic: Button
+    private lateinit var imageUri: Uri
+
+    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+        if (success) {
+            val imageView = findViewById<ImageView>(R.id.setting_profile_image)
+            imageView.setImageURI(imageUri) // Display the captured image
+        }
+    }
 
     private val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let {
@@ -43,31 +54,32 @@ class ProfileActivity : AppCompatActivity() {
     {
         val options = arrayOf("Camera", "Gallery")
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Pick Image Form")
+        builder.setTitle("Pick Image From")
 
-        builder.setItems(options) {
-            dialog: DialogInterface, which: Int ->
-            when(which)
-            {
-                0 -> {
-                    pickFromCamera()
-                }
-
-                1 -> {
-                    pickFromGallery()
-                }
+        builder.setItems(options) { _: DialogInterface, which: Int ->
+            when (which) {
+                0 -> pickFromCamera()
+                1 -> pickFromGallery()
             }
         }
-
         builder.create().show()
     }
 
+
     private fun pickFromCamera()
-    {}
+    {
+        val photoFile = createImageFile()
+        imageUri = FileProvider.getUriForFile(this, "$packageName.provider", photoFile)
+        cameraLauncher.launch(imageUri)
+    }
 
     private fun pickFromGallery(){
 
         galleryLauncher.launch("image/*")
 
+    }
+    private fun createImageFile(): File {
+        val storageDir = externalCacheDir
+        return File.createTempFile("IMG_", ".jpg", storageDir)
     }
 }
