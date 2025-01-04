2999b86bf7f025330e610ed4d06f68a5b89e0682
DulinaDineth1589 <dulinadineth2021@gmail.com>
2025-01-05 00:06:10 +0530

    hotfix

--- app/src/main/java/com/evergreen/eve/NotificationFragment.kt
+++ app/src/main/java/com/evergreen/eve/NotificationFragment.kt
@@ -1,32 +1,19 @@
 package com.evergreen.eve
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NotificationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +21,23 @@ class NotificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
-    }
+        val  view = inflater.inflate(R.layout.fragment_notification, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotificationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val listView: ListView = view.findViewById(R.id.notification_list)
+
+        val  notifications = listOf("notification 1", "notification 2", "notification 3", "notification 4", "notification 5")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, notifications)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, _, position, _ ->
+            val item = parent.getItemAtPosition(position) as String
+            Toast.makeText(requireContext(), "Clicked: $item", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+        return view
+
     }
 }
