14a8416256c3fa9345f0bd228b30f721713e4ac4
Sawindu Samuditha <sawindusamuditha@gmail.com>
2025-01-04 14:22:31 +0530

    Create settings fragment merch in main activity
    Create settings fragment and merch in main activity

--- app/src/main/java/com/evergreen/eve/SettingsFragment.kt
+++ app/src/main/java/com/evergreen/eve/SettingsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class SettingsFragment : Fragment() {
 
@@ -17,7 +20,21 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val  view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val listView: ListView = view.findViewById(R.id.listView)
+
+        val items = arrayListOf("Account", "Notifications", "Privacy", "Security", "Help", "About")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, _, position, _ ->
+            val item = parent.getItemAtPosition(position) as String
+            Toast.makeText(requireContext(), "Clicked: $item", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 
 
