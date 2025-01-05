dadc7ea404c3db6d405bbe131c116a2f43710cd0
Sawindu Samuditha <sawindusamuditha@gmail.com>
2025-01-05 11:38:17 +0530

    Update fertilizer kotlin class
    Update fertilizer calculation

--- app/src/main/java/com/evergreen/eve/FertilizerFragment.kt
+++ app/src/main/java/com/evergreen/eve/FertilizerFragment.kt
@@ -1,5 +1,6 @@
 package com.evergreen.eve
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
 
@@ -31,18 +33,44 @@ class FertilizerFragment : Fragment() {
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
         listView.adapter = adapter
 
+        val fetilizerInput:EditText = view.findViewById(R.id.txtfertilizersName)
+        val quantityInput:EditText = view.findViewById(R.id.txtQuantityAmount)
+        val applyButton = view.findViewById<Button>(R.id.btnApplyfertilizer)
+
         listView.setOnItemClickListener { parent, _, position, _ ->
             val fertilizer = parent.getItemAtPosition(position) as String
             Toast.makeText(requireContext(), "Clicked: $fertilizer", Toast.LENGTH_SHORT).show()
         }
 
-        val applyFertilizerButton: Button = view.findViewById(R.id.btnApplyfertilizer)
-        applyFertilizerButton.setOnClickListener {
-            val intent = Intent(requireContext(), FertilizerFragment::class.java)
-            startActivity(intent)
+
+        applyButton.setOnClickListener {
+            val nameFertilizer = fetilizerInput.text.toString()
+            val quantity = quantityInput.text.toString().toIntOrNull() ?: 0
+
+            val priceFetilizer = 12500
+
+            val totalAmount = priceFetilizer * quantity
+
+            val message = """
+                Fertilizer Name: $nameFertilizer
+                Quantity: $quantity
+                Total Amount: $totalAmount
+            """.trimIndent()
+
+            showDialog("Fertilizer Details", message)
         }
 
         return view
+    }
+
+    private fun showDialog(title: String, message: String) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
 }
